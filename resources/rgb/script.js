654be80f942296227649d9eb046b7dfc3d28ada7
Purpur <43528123+Pemigrade@users.noreply.github.com>
2024-01-08 20:59:48 -0600

    fix appeals

--- resources/rgb/script.js
+++ resources/rgb/script.js
@@ -292,7 +292,7 @@ function updateOutputText(event) {
   }
 
   if (newNick.trim() === 'birdflop.com') {
-    newNick = 'birdflop.com - $1.88/GB Minecraft Host';
+    newNick = 'birdflop.com';
   }
 
   const bold = document.getElementById('bold').checked;
